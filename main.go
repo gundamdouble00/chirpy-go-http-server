25b7ddb59ae8fc6c3f897bed6bd1d06a4b730511
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-05-08 12:58:55 +0700

    middleware and stateful handler

--- main.go
+++ main.go
@@ -1,13 +1,59 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+	"strconv"
+	"sync/atomic"
+)
 
-const port = ":8080"
+const port = "8080"
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func middlewareLog(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		_ = cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) handlerNumberOfRequests(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits = atomic.Int32{}
+	w.Write([]byte("Hits was reset"))
+}
 
 func main() {
-	serveMux := http.NewServeMux()
-	server := new(http.Server)
-	server.Handler = serveMux
-	server.Addr = port
-	server.ListenAndServe()
+	hanlder := http.StripPrefix("/app/", http.FileServer(http.Dir(".")))
+	mux := http.NewServeMux()
+	apiCfg := &apiConfig{}
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(hanlder))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+	mux.HandleFunc("/metrics", apiCfg.handlerNumberOfRequests)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
+
+	srv := &http.Server{
+		Addr:    ":" + port,
+		Handler: mux,
+	}
+	log.Printf("Serving on ports: %s\n", port)
+	log.Fatal(srv.ListenAndServe())
 }
