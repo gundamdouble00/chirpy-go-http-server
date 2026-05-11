79cc104a5645b9ac0ac449b711fae0c66528d5c1
gundamdouble00 <nguyenngocminhquocp@gmail.com>
2026-05-11 21:33:28 +0700

    JSON

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 	"strconv"
@@ -28,12 +30,62 @@ func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (cfg *apiConfig) handlerNumberOfRequests(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+	w.Header().Set("Content-Type", "text/html")
+	hits := strconv.Itoa(int(cfg.fileserverHits.Load()))
+	w.Write([]byte(fmt.Sprintf(`
+<html>
+	<body>
+		<h1>Welcome, Chirpy Admin</h1>
+		<p>Chirpy has been visited %s times!</p>
+	</body>
+</html>`, hits)))
 }
 
 func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits = atomic.Int32{}
-	w.Write([]byte("Hits was reset"))
+	// w.Write([]byte("Hits was reset"))
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	respondWithJSON(w, code, map[string]string{"error": msg})
+}
+
+func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
+	dat, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Cype", "application/json")
+	w.WriteHeader(code)
+	w.Write(dat)
+}
+
+func handlerValidateChirp(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		respondWithError(w, 400, "not supported method")
+		return
+	}
+
+	type requestBody struct {
+		Body string `json:"body"`
+	}
+
+	params := requestBody{}
+	err := json.NewDecoder(r.Body).Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, 200, map[string]bool{"valid": true})
 }
 
 func main() {
@@ -42,13 +94,16 @@ func main() {
 	apiCfg := &apiConfig{}
 
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(hanlder))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerNumberOfRequests)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
-	mux.HandleFunc("/metrics", apiCfg.handlerNumberOfRequests)
-	mux.HandleFunc("/reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/validate_chirp", handlerValidateChirp)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
